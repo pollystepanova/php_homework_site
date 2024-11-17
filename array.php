6ff78fe8ba5a4ac9f51668af190e962c736fad27
pollystepanova <saraslime57@gmail.com>
2024-11-17 21:44:19 +0300

    Add files via upload

---
+++ array.php
@@ -0,0 +1,3 @@
+<?php
+$array = ['users'=> ['login'=> 'polina', 'pass' => '123'], ['login'=> 'katy', 'pass' => '1290']];
+var_dump($_GET);
---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+//echo "Hello, world";
+//$time = time();
+
+require_once('time.html');
+require_once('array.php');
+
+//function OutputDlina(string $a): int
+//{
+ //   return strlen($a);
+//}
+//echo "<br>";
+//echo OutputDlina("mama");
